cfb22bf4ac8caac3c4a117fbb26832b0af1415cd
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2018-11-19 19:25:01 +0100

    Fix field type & chunk dump

--- import_dump.py
+++ import_dump.py
@@ -14,18 +14,24 @@ def iter_jsonl(jsonl_path):
 def import_dump(jsonl_path):
     inserts = []
 
+    rows = 0
     for item in iter_jsonl(jsonl_path):
         insert = {
             'id': str(uuid.uuid4()),
             'product_id': item['code'],
             'predicted_category': item['predicted_categories_tag'],
             'confidence': item['predicted_categories_prob'],
-            'last_updated_at': item['last_modified_t'],
+            'last_updated_at': str(item['last_modified_t']),
         }
         inserts.append(insert)
+        rows += 1
+
+        if rows % 100 == 0:
+            CategorizationTask.insert_many(inserts).execute()
+            inserts = []
 
     CategorizationTask.insert_many(inserts).execute()
 
 
 if __name__ == "__main__":
-    import_dump("/home/raphael/Projets/openfoodfacts-ui/predicted_categories.json")
+    import_dump("predicted_categories.json")
