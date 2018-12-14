2158203f5b9b5b4cc18e9ba15e11ae24b99a1d4d
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2018-12-14 10:40:37 +0100

    Add possibility to exclude product IDs from dump import

--- import_dump.py
+++ import_dump.py
@@ -11,14 +11,34 @@ def iter_jsonl(jsonl_path):
             yield json.loads(line)
 
 
-def import_dump(jsonl_path, campaign=None):
+def import_text_dataset(filepath):
+    data = []
+
+    with open(filepath, 'r') as f:
+        for line in f:
+            line = line.strip('\n')
+
+            if line:
+                data.append(line)
+
+    return data
+
+
+def import_dump(jsonl_path, campaign=None, exclude_ids=None):
     inserts = []
+
+    exclude_ids = exclude_ids or set()
 
     rows = 0
     for item in iter_jsonl(jsonl_path):
+        product_id = item['code']
+
+        if product_id in exclude_ids:
+            continue
+
         insert = {
             'id': str(uuid.uuid4()),
-            'product_id': item['code'],
+            'product_id': product_id,
             'predicted_category': item['predicted_category_tag'],
             'confidence': item.get('predicted_category_prob'),
             'last_updated_at': str(item['last_modified_t']),
@@ -45,9 +65,16 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="path of dump JSONL file")
     parser.add_argument("--campaign", help="name of the campaign")
+    parser.add_argument("--exclude-ids", help="filepath of IDs to exclude")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    import_dump(args.input, args.campaign)
+
+    exclude_ids = None
+
+    if args.exclude_ids:
+        exclude_ids = set(import_text_dataset(args.exclude_ids))
+
+    import_dump(args.input, args.campaign, exclude_ids)
