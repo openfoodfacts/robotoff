a58eec9c610ee834452488ccb61bc522734da309
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2018-12-17 13:44:00 +0100

    Use falcon for robotoff API

---
+++ robotoff/app/api.py
@@ -0,0 +1,72 @@
+import falcon
+from falcon_cors import CORS
+
+from robotoff.app.core import (generate_session_id,
+                               normalize_lang,
+                               get_next_product,
+                               parse_product_json,
+                               get_category_name,
+                               save_annotation)
+
+
+class CategoryPredictionResource:
+    def on_get(self, req, resp):
+        session_id = req.get_param('session_id')
+
+        response = {}
+
+        if session_id is None:
+            session_id = generate_session_id()
+            response['session_id'] = session_id
+
+        campaign = req.get_param('campaign')
+        lang = normalize_lang(req.get_param('lang'))
+
+        result = get_next_product(campaign)
+
+        if result is None:
+            response['status'] = "no_prediction_left"
+
+        else:
+            task, product = result
+            task.set_attribution(session_id=session_id)
+            response['product'] = parse_product_json(product, lang)
+            response['task_id'] = str(task.id)
+
+            predicted_category_name = get_category_name(task.predicted_category,
+                                                        lang)
+            response['prediction'] = {
+                'confidence': task.confidence,
+                'id': task.predicted_category,
+                'name': predicted_category_name,
+            }
+
+        resp.media = response
+
+
+class CategoryAnnotateResource:
+    def on_post(self, req, resp):
+        task_id = req.get_param('task_id', required=True)
+        annotation = req.get_param_as_int('annotation', required=True,
+                                          min=-1, max=1)
+
+        save = req.get_param_as_bool('save')
+
+        if save is None:
+            save = True
+
+        save_annotation(task_id, annotation, save=save)
+        resp.media = {
+            'status': 'saved',
+        }
+
+
+cors = CORS(allow_all_origins=True,
+            allow_all_headers=True,
+            allow_all_methods=True)
+
+api = falcon.API(middleware=[cors.middleware])
+# Parse form parameters
+api.req_options.auto_parse_form_urlencoded = True
+api.add_route('/api/v1/categories/predictions', CategoryPredictionResource())
+api.add_route('/api/v1/categories/annotate', CategoryAnnotateResource())
--- robotoff/app/web.py
+++ robotoff/app/web.py
@@ -1,7 +1,6 @@
 import logging
 
-from flask import Flask, request, render_template, session, jsonify
-from flask_cors import CORS
+from flask import Flask, request, render_template, session
 
 from robotoff.app.core import (generate_session_id,
                                get_next_product,
@@ -15,8 +14,6 @@ CORS(app)
 
 app.secret_key = b'k@\xcf\xb0\xfb\x94\xb2=3cJ7Q\xf1F\xd5'
 
-
-# Webapp
 
 def set_session_id():
     if 'session_id' not in session:
@@ -67,83 +64,6 @@ def categorize_post(campaign=None):
     annotation = int(request.form['annotation'])
     save_annotation(task_id, annotation)
     return render_next_product(campaign)
-
-
-# API
-
-@app.route('/api/v1/categories/predictions', methods=['GET'])
-def api_get_categories_prediction():
-    session_id = request.args.get('session_id')
-
-    response = {}
-
-    if session_id is None:
-        session_id = generate_session_id()
-        response['session_id'] = session_id
-
-    campaign = request.args.get('campaign')
-    lang = normalize_lang(request.args.get('lang'))
-
-    result = get_next_product(campaign)
-
-    if result is None:
-        response['status'] = "no_prediction_left"
-        return jsonify(response)
-
-    task, product = result
-    task.set_attribution(session_id=session_id)
-    response['product'] = parse_product_json(product, lang)
-    response['task_id'] = str(task.id)
-
-    predicted_category_name = get_category_name(task.predicted_category,
-                                                lang)
-    response['prediction'] = {
-        'confidence': task.confidence,
-        'id': task.predicted_category,
-        'name': predicted_category_name,
-    }
-
-    return jsonify(response)
-
-
-@app.route('/api/v1/categories/annotate', methods=['POST'])
-def api_submit_categories_annotation():
-    response = {}
-    task_id = request.form.get('task_id')
-
-    if task_id is None:
-        response['error'] = "invalid_task_id"
-        response['error_description'] = "The task_id parameter is required"
-        r = jsonify(response)
-        r.status_code = 404
-        return r
-
-    try:
-        annotation = int(request.form.get('annotation'))
-    except TypeError:
-        response['error'] = "invalid_annotation"
-        response['error_description'] = "The annotation parameter (in -1, 0, 1) is required"
-        r = jsonify(response)
-        r.status_code = 404
-        return r
-
-    save = request.form.get('save')
-    try:
-        if save is not None:
-            save = int(save)
-    except TypeError:
-        response['error'] = "invalid_save"
-        response['error_description'] = "The save parameter must be 1 or 0"
-        r = jsonify(response)
-        r.status_code = 404
-        return r
-
-    if save is None:
-        save = True
-
-    save_annotation(task_id, annotation, save=save)
-    response['status'] = 'saved'
-    return jsonify(response)
 
 
 if __name__ != '__main__':
