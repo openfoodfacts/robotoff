5d1335c0978f8461f0944a842d7ef4c646c95804
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-01-07 13:36:26 +0100

    Update index create script

--- es/create_index.sh
+++ es/create_index.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-INDEX='product'
-http PUT http://localhost:9200/${INDEX} < product_index.json
+http PUT http://localhost:9200/product < product_index.json
+http PUT http://localhost:9200/category < category_index.json
