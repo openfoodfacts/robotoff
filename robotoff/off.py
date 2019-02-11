91c8d4aba5a1315cf41c7eaf751769365309064b
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-02-11 17:42:40 +0100

    Add dry parameter to update_product function

--- robotoff/off.py
+++ robotoff/off.py
@@ -7,6 +7,7 @@ from robotoff.utils import get_logger
 http_session = requests.Session()
 
 POST_URL = "https://world.openfoodfacts.org/cgi/product_jqm2.pl"
+DRY_POST_URL = "https://world.openfoodfacts.net/cgi/product_jqm2.pl"
 AUTH = ("roboto-app", "4mbN9wJp8LBShcH")
 AUTH_DICT = {
     'user_id': AUTH[0],
@@ -42,37 +43,45 @@ def get_product(product_id: str, fields: List[str]=None):
     return data['product']
 
 
-def save_category(product_id: str, category: str):
+def save_category(product_id: str, category: str, dry=False):
     params = {
         'code': product_id,
         'add_categories': category,
         **AUTH_DICT
     }
-    update_product(params)
+    update_product(params, dry=dry)
 
 
-def save_ingredients(barcode: str, ingredient_text: str, lang: str=None):
-    ingredient_key = 'ingredients_text' if lang is None else f'ingredients_{lang}_text'
+def save_ingredients(barcode: str, ingredient_text: str,
+                     lang: str = None, dry=False):
+    ingredient_key = ('ingredients_text' if lang is None
+                      else f'ingredients_{lang}_text')
     params = {
         'code': barcode,
         ingredient_key: ingredient_text,
         **AUTH_DICT,
     }
-    update_product(params)
+    update_product(params, dry=dry)
 
 
-def add_emb_code(barcode: str, emb_code: str):
+def add_emb_code(barcode: str, emb_code: str, dry=False):
         params = {
             'code': barcode,
             'add_emb_codes': emb_code,
             'comment': "Adding packager code (automated edit)",
             **AUTH_DICT,
         }
-        update_product(params)
+        update_product(params, dry=dry)
 
 
-def update_product(params: Dict):
-    r = http_session.get(POST_URL, params=params)
+def update_product(params: Dict, dry=False):
+    if dry:
+        params.pop('password')
+        r = http_session.get(DRY_POST_URL, params=params,
+                             auth=('off', 'off'))
+    else:
+        r = http_session.get(POST_URL, params=params)
+
     r.raise_for_status()
     json = r.json()
 
