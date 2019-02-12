866ec10a613b92c067bffb1ebec11230c7cea719
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-02-12 15:51:21 +0100

    update batch annotation script

--- scripts/batch_annotate.py
+++ scripts/batch_annotate.py
@@ -6,9 +6,15 @@ insight_type = 'packager_code'
 annotator = InsightAnnotatorFactory.create(insight_type)
 
 
+i = 0
 for insight in ProductInsight.select().where(ProductInsight.type == insight_type,
                                              ProductInsight.annotation.is_null()):
-    print("Add emb code {} to https://fr.openfoodfacts.net/produit/{}".format(insight.data['text'], insight.barcode))
+    i += 1
+    print("Insight %d" % i)
+    if insight.data['matcher_type'] != 'eu_fr':
+        continue
+
+    print("Add emb code {} to https://fr.openfoodfacts.org/produit/{}".format(insight.data['text'], insight.barcode))
     print(insight.data)
     annotator.save_annotation(insight)
     insight.annotation = 1
