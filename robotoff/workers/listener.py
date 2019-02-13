ed547b8e2834912b3650d3633e10deb973167afd
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-02-13 23:18:36 +0100

    Update listener.py

--- robotoff/workers/listener.py
+++ robotoff/workers/listener.py
@@ -8,20 +8,22 @@ from robotoff.workers.tasks import run_task
 
 logger = get_logger()
 
-pool: Pool = Pool(settings.WORKER_COUNT)
 
-logger.info("Starting listener server")
+def run():
+    pool: Pool = Pool(settings.WORKER_COUNT)
 
-with Listener(settings.IPC_ADDRESS, authkey=settings.IPC_AUTHKEY) as listener:
-    while True:
-        logger.info("Waiting for a connection...")
+    logger.info("Starting listener server")
 
-        with listener.accept() as conn:
-            event = conn.recv()
-            event_type: str = event['type']
-            logger.info("New '{}' event received".format(event_type))
-            event_kwargs: Dict = event.get('meta', {})
+    with Listener(settings.IPC_ADDRESS, authkey=settings.IPC_AUTHKEY) as listener:
+        while True:
+            logger.info("Waiting for a connection...")
 
-            logger.info("Sending task to pool...")
-            result = pool.apply_async(run_task, (event_type, event_kwargs))
-            logger.info("Task sent")
+            with listener.accept() as conn:
+                event = conn.recv()
+                event_type: str = event['type']
+                logger.info("New '{}' event received".format(event_type))
+                event_kwargs: Dict = event.get('meta', {})
+
+                logger.info("Sending task to pool...")
+                pool.apply_async(run_task, (event_type, event_kwargs))
+                logger.info("Task sent")
