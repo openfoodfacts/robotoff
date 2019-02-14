2f09eca2e315e023b9676d281465cd59fa8fea1e
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-02-14 13:27:49 +0100

    Add new annotate-batch CLI subcommand

--- robotoff.py
+++ robotoff.py
@@ -32,9 +32,19 @@ def annotate(insight_type: Optional[str], country: Optional[str]):
     run(insight_type, country)
 
 
+@click.command()
+@click.option('--insight-type', required=True)
+@click.option('--dry/--no-dry', default=True)
+@click.option('-f', '--filter', 'filter_clause')
+def batch_annotate(insight_type: str, dry: bool, filter_clause: str):
+    from robotoff.cli.batch import run
+    run(insight_type, dry, filter_clause)
+
+
 cli.add_command(run)
 cli.add_command(generate_insights)
 cli.add_command(annotate)
+cli.add_command(batch_annotate)
 
 
 if __name__ == '__main__':
---
+++ robotoff/cli/batch.py
@@ -0,0 +1,57 @@
+import ast
+from typing import Dict, Optional
+
+from robotoff.insights.annotate import InsightAnnotatorFactory
+from robotoff.models import ProductInsight
+
+
+def run(insight_type: str,
+        dry: bool = True,
+        json_contains_str: Optional[str] = None):
+    if json_contains_str is not None:
+        json_contains = ast.literal_eval(json_contains_str)
+    else:
+        json_contains = None
+
+    batch_annotate(insight_type,
+                   dry,
+                   json_contains)
+
+
+def batch_annotate(insight_type: str,
+                   dry: bool = True,
+                   json_contains: Optional[Dict] = None):
+    annotator = InsightAnnotatorFactory.create(insight_type)
+
+    i = 0
+
+    query = ProductInsight.select()
+    where_clauses = [
+        ProductInsight.type == insight_type,
+        ProductInsight.annotation.is_null()
+    ]
+
+    if json_contains is not None:
+        where_clauses.append(ProductInsight.data.contains(json_contains))
+
+    query = query.where(*where_clauses)
+
+    if dry:
+        count = query.count()
+        print("-- dry run --\n"
+              "{} items matching filter:\n"
+              "   insight type: {}\n"
+              "   filter: {}"
+              "".format(count, insight_type, json_contains))
+    else:
+        for insight in query:
+            i += 1
+            print("Insight %d" % i)
+            print("Add label {} to https://fr.openfoodfacts.org/produit/{}"
+                  "".format(insight.data,
+                            insight.barcode))
+            print(insight.data)
+
+            annotator.save_annotation(insight)
+            insight.annotation = 1
+            insight.save()
