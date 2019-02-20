1cceb4be85aec9f3447746220cbfc79537d53de6
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-02-20 12:47:32 +0100

    Move ml module

--- robotoff/ml/category_classifier.py
+++ robotoff/ml/category_classifier.py
@@ -7,7 +7,8 @@ import networkx
 import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer, strip_accents_ascii
+from sklearn.feature_extraction.text import (TfidfTransformer, CountVectorizer,
+                                             strip_accents_ascii)
 
 from sklearn.linear_model import LogisticRegression
 
