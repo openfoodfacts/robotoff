77d1bf290ae00f5deda32607b1fe92e3379045a1
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-03-13 14:18:43 +0100

    Fix product_weight regex

--- robotoff/insights/ocr.py
+++ robotoff/insights/ocr.py
@@ -322,7 +322,7 @@ LABELS_REGEX = {
 }
 
 PRODUCT_WEIGHT_REGEX = OCRRegex(
-    re.compile(r"(poids net|poids net égoutté|volume net total|net weight|peso neto|peso liquido|netto[ -]?gewicht)\s?:?\s?([0-9]+[,.]?[0-9]*)\s?(fl oz|dle?|cle?|mge?|mle?|lbs|oz|ge?|kge?|le?)(?![^\s])"),
+    re.compile(r"(poids net|poids net égoutté|volume net total|net weight|peso neto|peso liquido|netto[ -]?gewicht)\s?:?\s?([0-9]+[,.]?[0-9]*)\s?(fl oz|dle?|cle?|mge?|mle?|lbs|oz|ge?|kge?|le?)(?![a-z])"),
     field=OCRField.full_text_contiguous,
     lowercase=True)
 
