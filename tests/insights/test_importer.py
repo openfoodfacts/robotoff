1b3e466605eb2c08f25542813eec56f007ea3554
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-04-01 16:32:39 +0200

    Fix unit test

--- tests/insights/test_importer.py
+++ tests/insights/test_importer.py
@@ -8,7 +8,7 @@ class TestBrandInsightImporter:
         ("5400141651306", "boni", True),
         ("5400142968395", "boni", False),
         ("3406790524499", "boni", False),
-        ("025252", "boni", True),
+        ("025252", "boni", False),
         ("3406790524499", "unknown-brand", True),
 
     ])
