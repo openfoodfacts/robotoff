0c60bbf817aa09623dc5dfde1d3ea5b0be30e458
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-09-11 20:45:53 +0200

    Version 1.0.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="robotoff",
-    version="1.0.7",
+    version="1.0.8",
     author="Openfoodfacts Team",
     description="Real-time and batch prediction service for Openfoodfacts",
     url="https://github.com/openfoodfacts/robotoff",
