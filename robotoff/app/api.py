f84ba0f70908a0ccd0fd793c6df23943c315eebd
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-11-18 22:34:41 +0100

    Strip URL path trailing slash

--- robotoff/app/api.py
+++ robotoff/app/api.py
@@ -434,6 +434,7 @@ api = falcon.API(middleware=[cors.middleware,
                              DBConnectionMiddleware()])
 # Parse form parameters
 api.req_options.auto_parse_form_urlencoded = True
+api.req_options.strip_url_path_trailing_slash = True
 api.add_route('/api/v1/insights/{barcode}', ProductInsightResource())
 api.add_route('/api/v1/insights/detail/{insight_id:uuid}', ProductInsightDetail())
 api.add_route('/api/v1/insights/random', RandomInsightResource())
