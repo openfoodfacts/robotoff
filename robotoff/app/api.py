30e54560b12148196c8e0c3ea048fecf87bdb31e
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-11-21 17:44:23 +0100

    Allow credentials on all origins

--- robotoff/app/api.py
+++ robotoff/app/api.py
@@ -427,7 +427,8 @@ class RandomQuestionsResource:
 
 cors = CORS(allow_all_origins=True,
             allow_all_headers=True,
-            allow_all_methods=True)
+            allow_all_methods=True,
+            allow_credentials_all_origins=True)
 
 api = falcon.API(middleware=[cors.middleware,
                              MultipartMiddleware(),
