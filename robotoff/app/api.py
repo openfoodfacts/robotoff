f17154ea3cc5d0140a8cb7d9fa5d714ca40095d3
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-12-01 19:09:24 +0100

    Cache CORS requests for 10 min

--- robotoff/app/api.py
+++ robotoff/app/api.py
@@ -428,7 +428,8 @@ class RandomQuestionsResource:
 cors = CORS(allow_all_origins=True,
             allow_all_headers=True,
             allow_all_methods=True,
-            allow_credentials_all_origins=True)
+            allow_credentials_all_origins=True,
+            max_age=600)
 
 api = falcon.API(middleware=[cors.middleware,
                              MultipartMiddleware(),
