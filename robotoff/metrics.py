b384956697b8007bfc4f795d13c76500b6fee476
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2020-03-07 19:02:21 +0100

    Save metric in InfluxDB

---
+++ robotoff/metrics.py
@@ -0,0 +1,77 @@
+import datetime
+from urllib.parse import urlparse
+from typing import List
+
+from influxdb import InfluxDBClient
+import requests
+
+from robotoff import settings
+
+URL_PATHS: List[str] = ["/ingredients-analysis?json=1", "/data-quality?json=1"]
+COUNTRY_TAGS = [
+    "world",
+    "us",
+    "uk",
+    "fr",
+    "es",
+    "it",
+    "be",
+    "nl",
+]
+
+
+def get_influx_client() -> InfluxDBClient:
+    return InfluxDBClient(
+        settings.INFLUXDB_HOST,
+        settings.INFLUXDB_PORT,
+        settings.INFLUXDB_USERNAME,
+        settings.INFLUXDB_PASSWORD,
+        settings.INFLUXDB_DB_NAME,
+    )
+
+
+def save_facet_metrics():
+    client = get_influx_client()
+
+    inserts = []
+    target_datetime = datetime.datetime.now()
+
+    for url_path in URL_PATHS:
+        for country_tag in COUNTRY_TAGS:
+            inserts += generate_metrics_from_path(
+                country_tag, url_path, target_datetime
+            )
+
+    client.write_points(inserts)
+
+
+def get_facet_name(url: str) -> str:
+    return urlparse(url)[2].strip("/").replace("-", "_")
+
+
+def generate_metrics_from_path(
+    country_tag: str, path: str, target_datetime: datetime.datetime
+) -> List:
+    url = f"https://{country_tag}-en.openfoodfacts.org{path}"
+    facet = get_facet_name(url)
+    data = requests.get(url).json()
+
+    inserts = []
+    for tag in data["tags"]:
+        name = tag["name"]
+        products = tag["products"]
+        tag_id = tag["id"]
+        inserts.append(
+            {
+                "measurement": "facets",
+                "tags": {
+                    "tag_name": name,
+                    "tag_id": tag_id,
+                    "country": country_tag,
+                    "facet": facet,
+                },
+                "time": target_datetime.isoformat(),
+                "fields": {"products": products},
+            }
+        )
+    return inserts
