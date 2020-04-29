2b6c1903b528be12d7b15140add6ed031d9b3b17
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2020-04-29 11:18:20 +0200

    Fix fiber quality facet generation

--- robotoff/scheduler/latent.py
+++ robotoff/scheduler/latent.py
@@ -1,7 +1,10 @@
+import pathlib
+
 from robotoff.insights._enum import InsightType
 from robotoff.models import LatentProductInsight
 from robotoff.products import get_product_store, DBProductStore
 from robotoff.utils import get_logger
+from robotoff.utils.types import JSONType
 
 logger = get_logger(__name__)
 
@@ -12,23 +15,40 @@ def generate_insights_from_latent_insights():
     generate_fiber_quality_facet()
 
 
+def is_valid_image(images: JSONType, image_path: str) -> bool:
+    image_id = pathlib.Path(image_path).stem
+    if not image_id.isdigit():
+        return False
+
+    return image_id in images
+
+
 def generate_fiber_quality_facet():
     product_store: DBProductStore = get_product_store()
     collection = product_store.collection
+    added = 0
 
     for latent_insight in LatentProductInsight.select(
-        LatentProductInsight.barcode
+        LatentProductInsight.barcode, LatentProductInsight.source_image
     ).where(
         LatentProductInsight.type == InsightType.nutrient_mention.name,
         LatentProductInsight.data["mentions"].contains("fiber"),
+        LatentProductInsight.source_image.is_null(False),
     ):
         barcode = latent_insight.barcode
-        product = product_store.get_product(barcode, ["fiber"])
-
-        if product is None or "fiber" in product:
+        product = product_store.get_product(
+            barcode, ["nutriments", "data_quality_tags", "images"]
+        )
+        if (
+            product is None
+            or not (product.get("images", {}), latent_insight.source_image)
+            or "fiber" in product.get("nutriments", {})
+            or FIBER_QUALITY_FACET_NAME in product.get("data_quality_tags", [])
+        ):
             continue
 
         logger.info("Adding {} facet to {}".format(FIBER_QUALITY_FACET_NAME, barcode))
+        added += 1
         collection.update_one(
             {"code": barcode},
             {
@@ -38,3 +58,6 @@ def generate_fiber_quality_facet():
                 }
             },
         )
+    logger.info(
+        "Quality facet {} added on {} products".format(FIBER_QUALITY_FACET_NAME, added)
+    )
