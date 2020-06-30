429e3339403bda6d1360600d8dd953bbdf7bf016
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2020-06-30 12:10:55 +0200

    Improve index generation script

--- services/ann/manage.py
+++ services/ann/manage.py
@@ -15,19 +15,28 @@ if __name__ == "__main__":
         import tempfile
 
         from annoy import AnnoyIndex
+        import tqdm
+
         from embeddings import EmbeddingStore
         import settings
+        from utils import get_logger
+
+        logger = get_logger()
 
         with tempfile.TemporaryDirectory() as tmp_dir:
             embedding_path = pathlib.Path(tmp_dir) / "embeddings.hdf5"
+            logger.info(f"Copying embedding file to {embedding_path}...")
             shutil.copy(str(settings.EMBEDDINGS_HDF5_PATH), str(embedding_path))
+
+            logger.info(f"Loading {embedding_path}...")
             embedding_store = EmbeddingStore(embedding_path)
 
             index = None
             offset: int = 0
             keys = []
 
-            for logo_id, embedding in embedding_store.iter_embeddings():
+            logger.info("Adding embeddings to index...")
+            for logo_id, embedding in tqdm.tqdm(embedding_store.iter_embeddings()):
                 if index is None:
                     output_dim = embedding.shape[-1]
                     index = AnnoyIndex(output_dim, "euclidean")
@@ -36,13 +45,19 @@ if __name__ == "__main__":
                 keys.append(int(logo_id))
                 offset += 1
 
+            logger.info("Building index...")
             if index is not None:
                 index.build(tree_count)
                 index.save(str(output))
+
+                logger.info("Index built.")
+                logger.info("Saving keys...")
 
                 with output.with_suffix(".txt").open("w") as f:
                     for key in keys:
                         f.write(str(key) + "\n")
 
-        cli.add_command(generate_index)
-        cli()
+                logger.info("Keys saved.")
+
+    cli.add_command(generate_index)
+    cli()
