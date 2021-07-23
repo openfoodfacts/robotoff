3217e5f0dbceafb9736a5cabb5d4318f273e2535
Yana K <kulizhsy@tcd.ie>
2021-07-23 14:04:38 +0200

    Run black on the change

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -10,7 +10,10 @@ from robotoff.settings import BaseURLProvider
         (BaseURLProvider().robotoff().get(), "https://robotoff.openfoodfacts.org"),
         (BaseURLProvider().country("fr").get(), "https://fr.openfoodfacts.org"),
         # In cases where multiple overrides are called, the last one takes precedence.
-        (BaseURLProvider().country("fr").robotoff().get(), "https://robotoff.openfoodfacts.org"),
+        (
+            BaseURLProvider().country("fr").robotoff().get(),
+            "https://robotoff.openfoodfacts.org",
+        ),
     ],
 )
 def test_base_url_provider(got_url, want_url):
