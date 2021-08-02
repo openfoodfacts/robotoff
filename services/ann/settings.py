d2a0775d05933cbd73c4a173b024c593392f4cb8
Yana K <kulizhsy@tcd.ie>
2021-08-02 17:27:11 +0200

    Parameterise Sentry initialisation to allow setting environments.

--- services/ann/settings.py
+++ services/ann/settings.py
@@ -1,11 +1,12 @@
 import os
 import pathlib
-from typing import Dict
+import sentry_sdk
+from sentry_sdk.integrations import Integration
+from typing import Dict, Sequence, Optional
 
 PROJECT_DIR = pathlib.Path(__file__).parent
 DATA_DIR = PROJECT_DIR / "data"
 
-SENTRY_DSN = os.environ.get("SENTRY_DSN")
 INDEX_DIM: Dict[str, int] = {"efficientnet-b0": 1280, "efficientnet-b5": 2048}
 IMAGE_INPUT_DIM: Dict[str, int] = {"efficientnet-b0": 224}
 
@@ -16,3 +17,26 @@ DEFAULT_INDEX = "efficientnet-b0"
 DEFAULT_MODEL = "efficientnet-b0"
 DEFAULT_HDF5_COUNT = 10000000
 EMBEDDINGS_HDF5_PATH = DATA_DIR / "efficientnet-b0.hdf5"
+
+# Should be either 'prod' or 'dev'.
+_ann_instance = os.environ.get("ANN_INSTANCE", "prod")
+
+if _ann_instance != "prod" and _ann_instance != "dev":
+    raise ValueError(
+        "ANN_INSTANCE should be either 'prod' or 'dev', got %s" % _ann_instance
+    )
+
+_sentry_dsn = os.environ.get("SENTRY_DSN")
+
+
+def init_sentry(integrations: Sequence[Integration] = None):
+    if _sentry_dsn:
+        sentry_sdk.init(
+            _sentry_dsn,
+            environment=_ann_instance,
+            integrations=integrations,
+        )
+    else:
+        raise ValueError(
+            "init_sentry was requested, yet SENTRY_DSN env variable was not provided"
+        )
