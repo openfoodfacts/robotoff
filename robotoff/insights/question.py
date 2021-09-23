f5311d0cf1867d876d8685210ba11a99b3b9191b
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-09-23 20:32:58 +0200

    correct url typo

--- robotoff/insights/question.py
+++ robotoff/insights/question.py
@@ -20,7 +20,7 @@ LABEL_IMG_BASE_URL = "https://{}/images/lang".format(
 )
 
 LABEL_IMAGES = {
-    "en:eu-organic": LABEL_IMG_BASE_URL + "en/labels/eu-organic.135x90.svg",
+    "en:eu-organic": LABEL_IMG_BASE_URL + "/en/labels/eu-organic.135x90.svg",
     "en:ab-agriculture-biologique": LABEL_IMG_BASE_URL
     + "/fr/labels/ab-agriculture-biologique.74x90.svg",
     "en:european-vegetarian-union": LABEL_IMG_BASE_URL
