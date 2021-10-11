28754cb82f0c0ebc50c581457078f9b69d06dcbb
Alex Garel <alex@garel.org>
2021-10-11 17:50:57 +0200

    ops: make it easier to run robotoff-cli in containers

--- docker/robotoff-entrypoint.sh
+++ docker/robotoff-entrypoint.sh
@@ -1,11 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 # activate our virtual environment here
 . /opt/pysetup/.venv/bin/activate
 
+PRE_ARGS=()
+
+# add poetry run for robotoff-cli, because we are lazy
+if [[ "$1" == "robotoff-cli" ]]
+then
+  PRE_ARGS=( poetry run )
+fi
+
 # You can put other setup logic here
 
 # Evaluating passed command:
-exec "$@"
+exec "${PRE_ARGS[@]}" "$@"
