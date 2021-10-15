51367fabbfec832acc873a35fbb857a7af9bf2df
autoblack <actions@github.com>
2021-10-15 15:11:14 +0000

    fixup: Format Python code with Black

--- tests/insights/ocr/test_core.py
+++ tests/insights/ocr/test_core.py
@@ -7,8 +7,7 @@ from robotoff.insights.ocr.core import extract_insights
 
 
 def test_extract_insights_category_deactivated(caplog):
-    """Category OCR prediction is deactivated for now
-    """
+    """Category OCR prediction is deactivated for now"""
     caplog.set_level(logging.INFO)
     result = extract_insights("spam", InsightType.category)
     assert result == []
@@ -17,7 +16,6 @@ def test_extract_insights_category_deactivated(caplog):
 
 
 def test_extract_insights_unknown_raises():
-    """If we extract an unknown insights it raises
-    """
+    """If we extract an unknown insights it raises"""
     with pytest.raises(ValueError, match="unknown insight type"):
         extract_insights("spam", InsightType.ingredient_spellcheck)
