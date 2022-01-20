5bebfab2a15b7a86f10c84d2222135df3a7e6d58
Alex Garel <alex@garel.org>
2022-01-20 17:31:50 +0100

    ops: fix health-check (#554)
    * ops: make docker-livecheck more robust
    
      should support service running multiple times 
      make it simpler, we can trust docker-compose
    * ops: wait a bit before health check on deploy
    
    * fix: fix health check
    
      to check right url for robotoff-ann
      and check robotoff on its own

--- docker/docker-livecheck.sh
+++ docker/docker-livecheck.sh
@@ -3,11 +3,13 @@
 ENV_FILE="${ENV_FILE:-.env}"
 RET_CODE=0
 for service in `docker-compose --env-file=${ENV_FILE} config  --service | tr '\n' ' '`; do 
-if [ -z `docker-compose ps -q $service` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose --env-file=${ENV_FILE} ps -q $service)` ]; then
-    echo "$service: DOWN"
-    RET_CODE=1
-else
-    echo "$service: UP"
-fi
+    found=$(docker-compose --env-file=${ENV_FILE} ps -- ${service}|grep  ${service}_1|grep '\bUp\b')
+    if [ -z "$found" ] 
+    then
+        echo "$service: DOWN"
+        RET_CODE=1
+    else
+        echo "$service: UP"
+    fi
 done;
 exit $RET_CODE;
