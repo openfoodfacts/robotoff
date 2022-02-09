e7bc0fb9b514c97837d4fe5bdfcf5fba832e7943
Mithridatea <mithridatea.local>
2022-02-09 14:49:06 +0100

    Update packaging tests

--- tests/unit/prediction/ocr/test_packaging.py
+++ tests/unit/prediction/ocr/test_packaging.py
@@ -4,6 +4,7 @@ import pytest
 
 from robotoff import settings
 from robotoff.prediction.ocr.packaging import find_packaging
+from robotoff.prediction.types import Prediction, PredictionType
 from robotoff.utils import text_file_iter
 
 
@@ -25,11 +26,20 @@ def test_packaging_format():
     [
         ("Tetrapack", {"tetra-pack"}),
         ("tetrapack", {"tetra-pack"}),
-        ("Packaging: boîte en carton,...", {"fr:boite-en-carton"}),
+        ("Packaging: boîte carton,...", {"fr:boite-en-carton"}),
         ("Ingrédients: 100% tomate", set()),
+        ("", set()),
+        (
+            "emballage: bouteille verre et son bouchon...",
+            {"fr:bouteille-en-verre", "fr:bouchon"},
+        ),
     ],
 )
 def test_find_packaging(text: str, value_tags: Set[str]):
-    insights = find_packaging(text)
-    detected_values = set(i.value_tag for i in insights)
+    predictions = find_packaging(text)
+    detected_values = set(i.value_tag for i in predictions)
     assert detected_values == value_tags
+
+    for prediction in predictions:
+        assert isinstance(prediction, Prediction)
+        assert prediction.type is PredictionType.packaging
