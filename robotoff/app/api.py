e78b42832408ea6808075496be2832f93b323f6b
Jagruti <tiwarijagruti1@gmail.com>
2022-08-27 10:56:55 +0530

    fix: adding order_by clause to the query

--- robotoff/app/api.py
+++ robotoff/app/api.py
@@ -1162,7 +1162,6 @@ class UnansweredQuestionCollection:
             response["questions"] = []
             response["status"] = "no_questions"
         else:
-            insights.sort(key=lambda x: x[1], reverse=True)
             response["questions"] = insights
             response["status"] = "found"
 
