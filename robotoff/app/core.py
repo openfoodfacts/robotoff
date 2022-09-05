54e1c7f24c3829b4a605a5fa7d1ec28eeda92792
Jagruti <tiwarijagruti1@gmail.com>
2022-09-05 21:39:37 +0530

    fix: sorted by ids

--- robotoff/app/core.py
+++ robotoff/app/core.py
@@ -134,8 +134,6 @@ def get_insights(
 
         elif order_by == "n_votes":
             query = query.order_by(ProductInsight.n_votes.desc())
-    else:
-        query = query.order_by(ProductInsight.id.desc())
 
     if as_dict:
         query = query.dicts()
@@ -204,7 +202,7 @@ def get_predictions(
     if where_clauses:
         query = query.where(*where_clauses)
 
-    # query = query.order_by(Prediction.timestamp.desc())
+    query = query.order_by(Prediction.id.desc())
 
     if count:
         return query.count()
@@ -245,7 +243,7 @@ def get_image_predictions(
     if where_clauses:
         query = query.where(*where_clauses)
 
-    # query = query.order_by(LogoAnnotation.image_prediction.timestamp.desc())
+    query = query.order_by(LogoAnnotation.image_prediction.id.desc())
 
     if count:
         return query.count()
@@ -369,7 +367,7 @@ def get_logo_annotation(
     if offset is not None:
         query = query.offset(offset)
 
-    # query = query.order_by(LogoAnnotation.image_prediction.timestamp.desc())
+    query = query.order_by(LogoAnnotation.image_prediction.id.desc())
 
     if count:
         return query.count()
