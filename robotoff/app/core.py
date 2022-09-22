213cf4dc66784781f5084ba727b4217bbfe8e763
Jagruti <tiwarijagruti1@gmail.com>
2022-09-22 20:58:05 +0530

    fix: fixed the test case

--- robotoff/app/core.py
+++ robotoff/app/core.py
@@ -100,8 +100,6 @@ def get_insights(
     if keep_types:
         where_clauses.append(ProductInsight.type.in_(keep_types))
 
-    # import pdb; pdb.set_trace()
-
     if country is not None:
         where_clauses.append(ProductInsight.countries.contains(country))
 
