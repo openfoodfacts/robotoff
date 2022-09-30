47fb0b75a4b14f1656fc5db5567914e70331220e
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2022-09-30 15:15:27 +0200

    feat: add new category matching algorithm

---
+++ robotoff/prediction/category/matcher.py
@@ -0,0 +1,347 @@
+from typing import Dict, Iterable, List, Optional, Set, Tuple
+import cachetools
+import datetime
+import re
+
+from flashtext import KeywordProcessor
+
+from robotoff.products import ProductDataset
+from robotoff.prediction.types import Prediction, PredictionType
+from robotoff import settings
+from robotoff.taxonomy import get_taxonomy, TaxonomyType
+from robotoff.utils.text import (
+    get_lemmatizing_nlp,
+    strip_accents_ascii,
+    strip_consecutive_spaces,
+)
+from robotoff.utils import dump_json, get_logger, load_json
+
+
+logger = get_logger(__name__)
+
+SUPPORTED_LANG = {"fr", "en", "de", "es", "it", "nl"}
+
+WEIGHT_REGEX = re.compile(
+    r"[0-9]+[,.]?[0-9]*\s?(fl oz|dl|cl|mg|ml|lbs|oz|g|kg|l)(?![a-z])"
+)
+
+LABELS_REGEX = {
+    "fr": re.compile(
+        r"agriculture biologique|biologique|bio|igp|aop|aoc|label rouge"
+        r"|commerce équitable"
+    ),
+    "en": re.compile(r"organic|pgi"),
+    "es": re.compile(r"ecológico|ecológica|extra|comercio justo"),
+    "de": re.compile(r"bio"),
+    "nl": re.compile(r"bio"),
+    "it": re.compile(
+        r"biologica|biologico|commercio solidale|commercio equo e solidale"
+        r"|da agricoltura biologica"
+    ),
+}
+
+EXTRAWORDS_REGEX = {
+    "fr": re.compile(r"gourmand|délicieux"),
+    "en": re.compile(r"delicious"),
+}
+
+STOP_WORDS_EXCEPTIONS = {
+    "fr": {"deux", "trois", "quatre"},
+    "en": {"two", "three", "four"},
+    "es": {"dos", "tres", "cuatro"},
+}
+
+
+def preprocess_product_name(name: str, lang: str) -> str:
+    """Preprocess product name before matching:
+    - remove all weight mentions (100 g, 1l,...)
+    - remove all label mentions (IGP, AOP, Label Rouge,...)
+    - remove uninformative words that lower recall
+
+    :param name: the product name
+    :param lang: the language of the product name
+    :return: the preprocessed product name
+    """
+    name = name.lower()
+    name = WEIGHT_REGEX.sub("", name)  # Remove weights
+
+    if lang in LABELS_REGEX:
+        # Remove labels
+        name = LABELS_REGEX[lang].sub("", name)
+
+    if lang in EXTRAWORDS_REGEX:
+        # Remove marketing words
+        name = EXTRAWORDS_REGEX[lang].sub("", name)
+
+    name = strip_consecutive_spaces(name)
+    name = name.strip()
+    return name
+
+
+def process(text: str, lang: str) -> str:
+    """Text processing pipeline for category matching.
+
+    Both category names from the taxonomy and product names should be
+    processed with this function before matching. The following steps are
+    performed:
+    - lowercasing
+    - language-specific stop word removal
+    - language-specific lookup-based lemmatization: fast and independent of
+    part of speech for speed and simplicity
+    - NFKD unicode normalization and accent stripping
+
+    :param text: text to process
+    :param lang: language of the text
+    :return: the processed string
+    """
+    nlp = get_lemmatizing_nlp(lang)
+    lemmas = []
+
+    # Savigny-lès-Beaune -> Savigny lès Beaune
+    text = text.lower().replace("-", " ")
+    doc = nlp(text)
+    lang_stop_words_exceptions = STOP_WORDS_EXCEPTIONS.get(lang, set())
+
+    for token in doc:
+        if (
+            (token.is_stop and token.orth_ not in lang_stop_words_exceptions)
+            or token.is_punct
+            or token.is_space
+        ):
+            continue
+        lemmas.append(token.lemma_)
+
+    return strip_accents_ascii(" ".join(lemmas))
+
+
+def generate_match_maps(
+    taxonomy_type: str,
+) -> Dict[str, Dict[str, List[Tuple[str, str]]]]:
+    """Return a dict mapping each supported lang to a match map dict.
+
+    Each match map dict maps a processed string to a list of
+    (value_tag, original_name) tuples. Queries are processed through
+    the `process` function. We have a match if the processed query is a key of
+    the map. It provides a very fast (although not memory-efficient) way of
+    performing matching.
+
+    :param taxonomy_type: the taxonomy to build the match maps from
+    :return: the match maps
+    """
+    taxonomy = get_taxonomy(taxonomy_type)
+    store: Dict[str, Dict[str, List[Tuple[str, str]]]] = {}
+    for node in taxonomy.iter_nodes():
+        for lang in set(node.names) | set(node.synonyms):
+            if lang not in SUPPORTED_LANG:
+                continue
+            store.setdefault(lang, {})
+
+            names = []
+            if lang in node.names:
+                names.append(node.names[lang])
+            names += node.synonyms.get(lang, [])
+
+            for name in names:
+                processed = process(name, lang)
+                # Don't add it to the store if empty
+                if processed:
+                    if processed not in store[lang]:
+                        store[lang][processed] = []
+                    if node.id not in (item[0] for item in store[lang][processed]):
+                        store[lang][processed].append((node.id, name))
+
+    return store
+
+
+@cachetools.cached(cache=cachetools.Cache(maxsize=2))
+def get_match_maps(taxonomy_type: str):
+    """Return match maps saved on-disk for supported language.
+
+    See `generate_match_maps` function for more information.
+    """
+    return load_json(
+        settings.CATEGORY_MATCHER_MATCH_MAPS[taxonomy_type], compressed=True
+    )
+
+
+@cachetools.cached(cache=cachetools.TTLCache(maxsize=1, ttl=3600))
+def get_processors() -> Dict[str, KeywordProcessor]:
+    """Return a dict mapping lang to KeywordProcessor used to perform category
+    matching."""
+    match_maps = get_match_maps(TaxonomyType.category.name)
+    processors = {}
+    for lang, items in match_maps.items():
+        processor = KeywordProcessor()
+        for pattern, results in items.items():
+            processor.add_keyword(pattern, [(id_, pattern) for id_, _ in results])
+        processors[lang] = processor
+    return processors
+
+
+def generate_intersect_categories_ingredients() -> Dict[str, Set[str]]:
+    """Return all taxonomized values that are present both in the ingredient
+    and in the category taxonomy.
+
+    We use match maps from both taxonomies to find names/synonyms that are the
+    same in both taxonomies.
+    This function is useful to spot categories that are also ingredients.
+    """
+    ingredient_match_maps = get_match_maps(TaxonomyType.ingredient.name)
+    category_match_maps = get_match_maps(TaxonomyType.category.name)
+    matches: Dict[str, Set[str]] = {}
+    for lang in SUPPORTED_LANG:
+        matches[lang] = set()
+
+    for lang in set(ingredient_match_maps) | set(category_match_maps):
+        for key in set(ingredient_match_maps[lang]) & set(category_match_maps[lang]):
+            for item in category_match_maps[lang][key]:
+                matches[lang].add(item[0])  # Add node ID
+
+    return matches
+
+
+@cachetools.cached(cache=cachetools.Cache(maxsize=1))
+def get_intersect_categories_ingredients():
+    """Return intersection between category and ingredient maps saved on-disk
+    for supported language.
+
+    See `generate_intersect_categories_ingredients` function for more
+    information.
+    """
+    return {
+        k: set(v)
+        for k, v in load_json(
+            settings.CATEGORY_MATCHER_INTERSECT, compressed=True
+        ).items()
+    }
+
+
+def match(query: str, lang: str) -> List[Tuple[str, str]]:
+    """Perform category matching.
+
+    If a match is also an ingredient (see
+    `generate_intersect_categories_ingredients`), we only keep the match if
+    it starts at the beginning of the query. It prevents many over-matchings
+    with ingredient names in the query that are not the product category.
+
+    :param query: the unprocessed category name
+    :param lang: the language of the query
+    :return A list of matches (category_value_tag, matched_pattern)
+    """
+    processors = get_processors()
+    if lang not in processors:
+        return []
+
+    processor = processors[lang]
+    query = preprocess_product_name(query, lang)
+    query = process(query, lang)
+    flat_results = []
+    for results, start_idx, end_idx in processor.extract_keywords(
+        query, span_info=True
+    ):
+        for result in results:
+            flat_results.append((result, start_idx, end_idx))
+    filtered = []
+    category_ingredient_intersect = get_intersect_categories_ingredients()[lang]
+    for (id_, pattern), start_idx, _ in flat_results:
+        # If the category is also an ingredient, we require the match
+        # to start at the beginning of the query
+        # This prevent for instance "pineapple" category to match
+        # "pizza with pineapple" product name
+        if id_ in category_ingredient_intersect and start_idx != 0:
+            continue
+        filtered.append((id_, pattern))
+    return filtered
+
+
+def predict(product: Dict) -> Optional[Prediction]:
+    """Predict a product category using a matching algorithm.
+
+    Flashtext always return the longuest match if several pattern match the
+    query. Therefore, if more than one category is predicted, it means there
+    was two non-overlapping matches: we don't return any prediction in that
+    case.
+
+    :param product: product properties
+    :return: a category Prediction or None if no prediction was available
+    """
+    for lang in set(product.get("languages_codes", [])) & SUPPORTED_LANG:
+        product_name = product.get(f"product_name_{lang}")
+
+        if not product_name:
+            continue
+
+        matches = match(product_name, lang)
+
+        if len(matches) != 1:
+            # if there is more than one match, matches on this product name
+            # are ambiguous and we discard them
+            continue
+
+        prediction = matches[0]
+
+        return Prediction(
+            type=PredictionType.category,
+            barcode=product["code"],
+            value_tag=prediction[0],
+            data={
+                "lang": lang,
+                "product_name": product_name,
+                "pattern": prediction[1],
+            },
+            automatic_processing=False,
+            predictor="matcher",
+        )
+
+    return None
+
+
+def predict_from_dataset(
+    dataset: ProductDataset, from_datetime: Optional[datetime.datetime] = None
+) -> Iterable[Prediction]:
+    """Return an iterable of category predictions, using the provided dataset.
+
+    Args:
+        dataset: a ProductDataset
+        from_datetime: datetime threshold: only keep products modified after
+            `from_datetime`
+    """
+    product_stream = (
+        dataset.stream()
+        .filter_nonempty_text_field("code")
+        .filter_nonempty_text_field("product_name")
+        .filter_empty_tag_field("categories_tags")
+        .filter_nonempty_tag_field("countries_tags")
+        .filter_nonempty_tag_field("languages_codes")
+    )
+
+    if from_datetime:
+        product_stream = product_stream.filter_by_modified_datetime(
+            from_t=from_datetime
+        )
+
+    logger.info("Performing prediction on products without categories")
+    for product in product_stream.iter():
+        prediction = predict(product)
+
+        if prediction:
+            yield prediction
+
+
+def dump_match_files():
+    """Generate and save on-disk match maps (ingredient and category) and the
+    category-ingredient intersection."""
+    for taxonomy_type in (TaxonomyType.ingredient.name, TaxonomyType.category.name):
+        match_map = generate_match_maps(taxonomy_type)
+        dump_json(
+            settings.CATEGORY_MATCHER_MATCH_MAPS[taxonomy_type],
+            match_map,
+            compressed=True,
+        )
+
+    intersect = generate_intersect_categories_ingredients()
+    dump_json(
+        settings.CATEGORY_MATCHER_INTERSECT,
+        {k: list(v) for k, v in intersect.items()},
+        compressed=True,
+    )
---
+++ tests/unit/prediction/category/test_matcher.py
@@ -0,0 +1,105 @@
+import pytest
+from typing import List
+
+from robotoff.prediction.category.matcher import match, predict, process
+from robotoff.prediction.types import Prediction, PredictionType
+from robotoff.taxonomy import get_taxonomy
+
+
+@pytest.mark.parametrize(
+    "text,lang,expected",
+    [
+        ("Filets de POULET", "fr", "filet poulet"),
+        ("poulets au curry", "fr", "poulet curry"),
+        ("Mélange quatre épices", "fr", "melanger quatre epice"),
+        ("of", "en", ""),  # of is a stop word in en
+    ],
+)
+def test_process(text: str, lang: str, expected: str):
+    assert process(text, lang) == expected
+
+
+@pytest.mark.parametrize(
+    "text,lang,expected",
+    [
+        (
+            "Filets de POULET bio de provence",
+            "fr",
+            ["en:chicken-breasts"],
+        ),
+        ("poulets au curry épicé", "fr", ["en:chicken-curry"]),
+        (
+            "yaourt nature et riz au lait",
+            "fr",
+            ["en:plain-yogurts", "en:rice-puddings"],
+        ),
+        ("Sprite", "fr", []),  # no category named sprite
+        (
+            "Sirop de mangue",
+            "fr",
+            ["en:mango-syrups"],
+        ),
+        ("Lardons pauvres en sel", "fr", ["en:lardons"]),
+        ("miels Méditérannée", "fr", ["en:mediterannean-honeys"]),
+        ("yaourt nature", "fr", ["en:plain-yogurts"]),
+        ("eaux de sources Cristaline 1,5L", "fr", ["en:spring-waters"]),
+        ("crème dessert vanille", "fr", ["fr:cremes-dessert-vanille"]),
+        ("UNKNOWN", "en", []),
+        ("PARIS Brest", "en", ["en:paris-brest"]),
+        ("Asian Chicken Salad", "en", []),
+        ("Chorizo de pavo", "es", ["en:chorizo"]),
+        ("Pollo limpio", "es", ["en:chickens"]),
+    ],
+)
+def test_match(text: str, lang: str, expected: List[str], mocker):
+    mocker.patch(
+        "robotoff.prediction.category.matcher.get_taxonomy",
+        return_value=get_taxonomy("category", offline=True),
+    )
+    results = match(text, lang)
+    assert [result[0] for result in results] == expected
+
+
+@pytest.mark.parametrize(
+    "lang,product_name,expected_value_tag,expected_pattern",
+    [
+        (
+            "fr",
+            "Aiguillettes de canard du pays basque",
+            "fr:aiguillettes-de-canard",
+            "aiguillette canard",
+        ),
+        # More than one category matching this query so no result should be
+        # returned
+        (
+            "fr",
+            "yaourt nature et riz au lait",
+            None,
+            None,
+        ),
+    ],
+)
+def test_predict(
+    lang: str, product_name: str, expected_value_tag: str, expected_pattern: str
+):
+    product = {
+        "code": "0000",
+        f"product_name_{lang}": product_name,
+        "languages_codes": [lang],
+    }
+    prediction = predict(product)
+
+    if expected_value_tag is None:
+        assert prediction is None
+    else:
+        assert isinstance(prediction, Prediction)
+        assert prediction.type == PredictionType.category.name
+        assert prediction.value_tag == expected_value_tag
+        assert prediction.predictor == "matcher"
+        assert prediction.data == {
+            "pattern": expected_pattern,
+            "product_name": product_name,
+            "lang": lang,
+        }
+        assert prediction.barcode == product["code"]
+        assert prediction.automatic_processing is False
