5e00207f4a761ee640560d4623ccad943fd8d1c8
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2022-10-18 17:38:30 +0200

    fix: rename image_url field into ref_image_url
    besides:
    - store the label->logo image URL mapping in a JSON file
    - add new logos
    - fix incorrect logo image URL for all label logos

--- robotoff/settings.py
+++ robotoff/settings.py
@@ -77,6 +77,7 @@ DATA_DIR = PROJECT_DIR / "data"
 DATASET_DIR = PROJECT_DIR / "datasets"
 DATASET_DIR.mkdir(exist_ok=True)
 I18N_DIR = PROJECT_DIR / "i18n"
+LABEL_LOGOS_PATH = DATA_DIR / "label_logos.json"
 JSONL_DATASET_PATH = DATASET_DIR / "products.jsonl.gz"
 JSONL_DATASET_ETAG_PATH = DATASET_DIR / "products-etag.txt"
 JSONL_MIN_DATASET_PATH = DATASET_DIR / "products-min.jsonl.gz"
