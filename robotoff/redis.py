61e77b068befa089c9e9983d76b5e1e9a3a8c587
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2022-12-01 18:16:40 +0100

    feat: add redis lock to avoid concurrent insight import for the same product

--- robotoff/redis.py
+++ robotoff/redis.py
@@ -1,5 +1,41 @@
-from redis import Redis
+from typing import Optional
 
+from redis import Redis
+from redis_lock import Lock as BaseLock
 from robotoff import settings
 
 redis_conn = Redis(host=settings.REDIS_HOST)
+
+
+class LockedResourceException(Exception):
+    pass
+
+
+class Lock(BaseLock):
+    _enabled = True
+
+    def __init__(
+        self,
+        name: str,
+        blocking: bool = False,
+        timeout: Optional[float] = None,
+        expire: int = 60,
+        **kwargs,
+    ):
+        self.timeout = timeout
+        if timeout is not None:
+            blocking = True
+        self.blocking = blocking
+        if self._enabled:
+            super().__init__(redis_conn, name=name, expire=expire, **kwargs)
+
+    def __enter__(self):
+        if self._enabled:
+            acquired = self.acquire(blocking=self.blocking, timeout=self.timeout)
+            if not acquired:
+                raise LockedResourceException()
+            return self
+
+    def __exit__(self, *args, **kwargs):
+        if self._enabled:
+            self.release()
