2d96e7a2218c484627b1ed3202002ae06ff2e082
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2022-12-12 16:55:33 +0100

    fix: remove legacy script comments

--- build_mkdocs.sh
+++ build_mkdocs.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Script to safely download and uncompress Nutriscore model from Github.
-# Usage: run `sh ./deploy/download-nutriscore.sh` from root directory.
-#
-# If the template is already in place, no action is performed. If the 
-# model does not exist, it is downloaded and unzipped. The .tar.gz 
-# file is then deleted.
-
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
