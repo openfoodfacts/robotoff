openfoodfacts/robotoff

commit 5178bac94a6be1ad6a7fb5be0d9faeb641bf3345

fix: improve BaseURLProvider class
BaseURLProvider calls were too verbose, and a change to .env file locally
made test_question.py test failed.

robotoff/app/api.py
@@ -478,7 +478,7 @@ class ImageImporterResource:
         ocr_url = req.get_param("ocr_url", required=True)
         server_domain = req.get_param("server_domain", required=True)
 
-        if server_domain != settings.OFF_SERVER_DOMAIN:
+        if server_domain != settings.BaseURLProvider.api():
             logger.info("Rejecting image import from %s", server_domain)
             resp.media = {
                 "status": "rejected",
@@ -529,7 +529,7 @@ class ImagePredictionImporterResource:
 
         for prediction in req.media["predictions"]:
             server_domain: str = prediction.get(
-                "server_domain", settings.OFF_SERVER_DOMAIN
+                "server_domain", settings.BaseURLProvider.api()
             )
             server_type: str = get_server_type(server_domain).name
             source_image = generate_image_path(
@@ -802,7 +802,7 @@ class ImageLogoDetailResource:
                     completed_at=datetime.datetime.utcnow(),
                 )
                 generate_insights_from_annotated_logos(
-                    annotated_logos, settings.OFF_SERVER_DOMAIN, auth
+                    annotated_logos, settings.BaseURLProvider.api(), auth
                 )
 
         resp.status = falcon.HTTP_204
@@ -866,7 +866,7 @@ class ImageLogoAnnotateResource:
             raise falcon.HTTPForbidden(
                 description="authentication is required to annotate logos"
             )
-        server_domain = req.media.get("server_domain", settings.OFF_SERVER_DOMAIN)
+        server_domain = req.media.get("server_domain", settings.BaseURLProvider.api())
         annotations = req.media["annotations"]
         completed_at = datetime.datetime.utcnow()
         annotation_logos = []
@@ -992,7 +992,7 @@ class WebhookProductResource:
         barcode = req.get_param("barcode", required=True)
         action = req.get_param("action", required=True)
         server_domain = req.get_param("server_domain", required=True)
-        if server_domain != settings.OFF_SERVER_DOMAIN:
+        if server_domain != settings.BaseURLProvider.api():
             logger.info("Rejecting webhook event from {}".format(server_domain))
             resp.media = {
                 "status": "rejected",
@@ -1257,7 +1257,7 @@ class ImageCollection:
             "with_predictions", default=False
         )
         barcode: Optional[str] = req.get_param("barcode")
-        server_domain = settings.OFF_SERVER_DOMAIN
+        server_domain = settings.BaseURLProvider.api()
 
         get_images_ = functools.partial(
             get_images,

robotoff/app/core.py
@@ -87,7 +87,7 @@ def get_insights(
     predictor: Optional[str] = None,
 ) -> Iterable[ProductInsight]:
     if server_domain is None:
-        server_domain = settings.OFF_SERVER_DOMAIN
+        server_domain = settings.BaseURLProvider.api()
 
     where_clauses = [ProductInsight.server_domain == server_domain]
 
@@ -176,7 +176,7 @@ def get_images(
     limit: Optional[int] = 25,
 ) -> Iterable[ImageModel]:
     if server_domain is None:
-        server_domain = settings.OFF_SERVER_DOMAIN
+        server_domain = settings.BaseURLProvider.api()
 
     where_clauses = [ImageModel.server_domain == server_domain]
 
@@ -210,7 +210,7 @@ def get_predictions(
     count: bool = False,
 ) -> Iterable[Prediction]:
     if server_domain is None:
-        server_domain = settings.OFF_SERVER_DOMAIN
+        server_domain = settings.BaseURLProvider.api()
 
     where_clauses = [Prediction.server_domain == server_domain]
 
@@ -249,7 +249,7 @@ def get_image_predictions(
     query = ImagePrediction.select()
 
     if server_domain is None:
-        server_domain = settings.OFF_SERVER_DOMAIN
+        server_domain = settings.BaseURLProvider.api()
 
     query = query.switch(ImagePrediction).join(ImageModel)
     where_clauses = [ImagePrediction.image.server_domain == server_domain]
@@ -404,7 +404,7 @@ def get_logo_annotation(
 ) -> Iterable[LogoAnnotation]:
 
     if server_domain is None:
-        server_domain = settings.OFF_SERVER_DOMAIN
+        server_domain = settings.BaseURLProvider.api()
 
     query = LogoAnnotation.select().join(ImagePrediction).join(ImageModel)
 

robotoff/cli/main.py
@@ -73,7 +73,7 @@ def regenerate_ocr_insights(
 
     with db:
         import_result = importer.import_insights(
-            predictions, settings.OFF_SERVER_DOMAIN
+            predictions, settings.BaseURLProvider.api()
         )
         logger.info(import_result)
 
@@ -208,7 +208,7 @@ def import_insights(
             # Create a new transaction for every batch
             with db.atomic():
                 import_results = importer.import_insights(
-                    prediction_batch, settings.OFF_SERVER_DOMAIN
+                    prediction_batch, settings.BaseURLProvider.api()
                 )
                 logger.info(import_results)
 
@@ -245,7 +245,7 @@ def refresh_insights(
     if barcode is not None:
         logger.info(f"Refreshing product {barcode}")
         with db:
-            imported = refresh_insights_(barcode, settings.OFF_SERVER_DOMAIN)
+            imported = refresh_insights_(barcode, settings.BaseURLProvider.api())
         logger.info(f"Refreshed insights: {imported}")
     else:
         logger.info("Launching insight refresh on full database")
@@ -272,7 +272,7 @@ def refresh_insights(
                 low_queue,
                 job_kwargs={"result_ttl": 0, "timeout": "5m"},
                 barcodes=barcode_batch,
-                server_domain=settings.OFF_SERVER_DOMAIN,
+                server_domain=settings.BaseURLProvider.api(),
             )
 
 
@@ -323,7 +323,7 @@ def import_images_in_db(
                 low_queue,
                 job_kwargs={"result_ttl": 0},
                 batch=batch,
-                server_domain=settings.OFF_SERVER_DOMAIN,
+                server_domain=settings.BaseURLProvider.api(),
             )
 
 
@@ -415,7 +415,7 @@ def run_object_detection_model(
                 job_kwargs={"result_ttl": 0},
                 barcode=barcode,
                 image_url=image_url,
-                server_domain=settings.OFF_SERVER_DOMAIN,
+                server_domain=settings.BaseURLProvider.api(),
             )
 
 

robotoff/logos.py
@@ -527,7 +527,7 @@ def refresh_nearest_neighbors(day_offset: int = 7, batch_size: int = 500):
                 import_logo_insights(
                     logos,
                     thresholds=thresholds,
-                    server_domain=settings.OFF_SERVER_DOMAIN,
+                    server_domain=settings.BaseURLProvider.api(),
                     notify=False,
                 )
 

robotoff/off.py
@@ -71,7 +71,7 @@ class ServerType(enum.Enum):
 
 
 API_URLS: dict[ServerType, str] = {
-    ServerType.off: settings.BaseURLProvider().get(),
+    ServerType.off: settings.BaseURLProvider.world(),
     ServerType.obf: "https://world.openbeautyfacts.org",
     ServerType.opf: "https://world.openproductfacts.org",
     ServerType.opff: "https://world.openpetfoodfacts.org",
@@ -126,7 +126,7 @@ def get_base_url(server: Union[ServerType, str]) -> str:
     if isinstance(server, str):
         server = server.replace("api", "world")
         # get scheme, https on prod, but http in dev
-        scheme = settings.BaseURLProvider().scheme
+        scheme = settings._get_default_scheme()
         return f"{scheme}://{server}"
     else:
         if server not in API_URLS:
@@ -178,11 +178,11 @@ def generate_json_path(barcode: str, image_id: str) -> str:
 
 
 def generate_json_ocr_url(barcode: str, image_id: str) -> str:
-    return settings.OFF_IMAGE_BASE_URL + generate_json_path(barcode, image_id)
+    return settings.BaseURLProvider.image_url(generate_json_path(barcode, image_id))
 
 
 def generate_image_url(barcode: str, image_id: str) -> str:
-    return settings.OFF_IMAGE_BASE_URL + generate_image_path(barcode, image_id)
+    return settings.BaseURLProvider.image_url(generate_image_path(barcode, image_id))
 
 
 def is_valid_image(barcode: str, image_id: str) -> bool:
@@ -387,7 +387,7 @@ def update_product(
     timeout: Optional[int] = 15,
 ):
     if server_domain is None:
-        server_domain = settings.OFF_SERVER_DOMAIN
+        server_domain = settings.BaseURLProvider.api()
 
     url = get_product_update_url(server_domain)
 
@@ -433,7 +433,7 @@ def move_to(barcode: str, to: ServerType, timeout: Optional[int] = 10) -> bool:
     if get_product(barcode, server=to) is not None:
         return False
 
-    url = "{}/cgi/product_jqm.pl".format(settings.BaseURLProvider().get())
+    url = "{}/cgi/product_jqm.pl".format(settings.BaseURLProvider.world())
     params = {
         "type": "edit",
         "code": barcode,
@@ -455,7 +455,7 @@ def select_rotate_image(
     timeout: Optional[int] = 15,
 ):
     if server_domain is None:
-        server_domain = settings.OFF_SERVER_DOMAIN
+        server_domain = settings.BaseURLProvider.api()
 
     url = get_product_image_select_url(server_domain)
     cookies = None

robotoff/scheduler/__init__.py
@@ -100,7 +100,7 @@ def refresh_insights(with_deletion: bool = False):
         .where(
             ProductInsight.annotation.is_null(),
             ProductInsight.timestamp <= datetime_threshold,
-            ProductInsight.server_domain == settings.OFF_SERVER_DOMAIN,
+            ProductInsight.server_domain == settings.BaseURLProvider.api(),
         )
         .iterator()
     ):
@@ -217,7 +217,8 @@ def generate_insights():
 
     with db:
         import_result = import_insights(
-            product_predictions_iter, server_domain=settings.OFF_SERVER_DOMAIN
+            product_predictions_iter,
+            server_domain=settings.BaseURLProvider.api(),
         )
     logger.info(import_result)
 

robotoff/scheduler/latent.py
@@ -102,7 +102,7 @@ def get_image_orientation(barcode: str, image_id: str) -> Optional[int]:
         .where(
             Prediction.barcode == barcode,
             Prediction.type == PredictionType.image_orientation.name,
-            Prediction.server_domain == settings.OFF_SERVER_DOMAIN,
+            Prediction.server_domain == settings.BaseURLProvider.api(),
             Prediction.source_image.is_null(False),
         )
         .iterator()
@@ -123,7 +123,7 @@ def generate_nutrition_image_insights():
         .where(
             ProductInsight.annotation.is_null(),
             ProductInsight.type == InsightType.nutrition_image.name,
-            ProductInsight.server_domain == settings.OFF_SERVER_DOMAIN,
+            ProductInsight.server_domain == settings.BaseURLProvider.api(),
         )
         .execute()
     )
@@ -171,7 +171,7 @@ def generate_nutrition_image_insights():
                         Prediction.type == PredictionType.nutrition_image.name,
                         Prediction.barcode == barcode,
                         Prediction.value_tag == lang,
-                        Prediction.server_domain == settings.OFF_SERVER_DOMAIN,
+                        Prediction.server_domain == settings.BaseURLProvider.api(),
                     )
                     .count()
                 ):

commit 80730e4437c210216a7895fd9eb6148d55419115

fix: fix server_domain bug
introduced by 5178bac94a6be1ad6a7fb5be0d9faeb641bf3345

robotoff/app/api.py
@@ -478,7 +478,7 @@ class ImageImporterResource:
         ocr_url = req.get_param("ocr_url", required=True)
         server_domain = req.get_param("server_domain", required=True)
 
-        if server_domain != settings.BaseURLProvider.api():
+        if server_domain != settings.BaseURLProvider.server_domain():
             logger.info("Rejecting image import from %s", server_domain)
             resp.media = {
                 "status": "rejected",
@@ -529,7 +529,7 @@ class ImagePredictionImporterResource:
 
         for prediction in req.media["predictions"]:
             server_domain: str = prediction.get(
-                "server_domain", settings.BaseURLProvider.api()
+                "server_domain", settings.BaseURLProvider.server_domain()
             )
             server_type: str = get_server_type(server_domain).name
             source_image = generate_image_path(
@@ -802,7 +802,7 @@ class ImageLogoDetailResource:
                     completed_at=datetime.datetime.utcnow(),
                 )
                 generate_insights_from_annotated_logos(
-                    annotated_logos, settings.BaseURLProvider.api(), auth
+                    annotated_logos, settings.BaseURLProvider.server_domain(), auth
                 )
 
         resp.status = falcon.HTTP_204
@@ -866,7 +866,9 @@ class ImageLogoAnnotateResource:
             raise falcon.HTTPForbidden(
                 description="authentication is required to annotate logos"
             )
-        server_domain = req.media.get("server_domain", settings.BaseURLProvider.api())
+        server_domain = req.media.get(
+            "server_domain", settings.BaseURLProvider.server_domain()
+        )
         annotations = req.media["annotations"]
         completed_at = datetime.datetime.utcnow()
         annotation_logos = []
@@ -992,7 +994,7 @@ class WebhookProductResource:
         barcode = req.get_param("barcode", required=True)
         action = req.get_param("action", required=True)
         server_domain = req.get_param("server_domain", required=True)
-        if server_domain != settings.BaseURLProvider.api():
+        if server_domain != settings.BaseURLProvider.server_domain():
             logger.info("Rejecting webhook event from {}".format(server_domain))
             resp.media = {
                 "status": "rejected",
@@ -1257,7 +1259,7 @@ class ImageCollection:
             "with_predictions", default=False
         )
         barcode: Optional[str] = req.get_param("barcode")
-        server_domain = settings.BaseURLProvider.api()
+        server_domain = settings.BaseURLProvider.server_domain()
 
         get_images_ = functools.partial(
             get_images,

robotoff/app/core.py
@@ -87,7 +87,7 @@ def get_insights(
     predictor: Optional[str] = None,
 ) -> Iterable[ProductInsight]:
     if server_domain is None:
-        server_domain = settings.BaseURLProvider.api()
+        server_domain = settings.BaseURLProvider.server_domain()
 
     where_clauses = [ProductInsight.server_domain == server_domain]
 
@@ -176,7 +176,7 @@ def get_images(
     limit: Optional[int] = 25,
 ) -> Iterable[ImageModel]:
     if server_domain is None:
-        server_domain = settings.BaseURLProvider.api()
+        server_domain = settings.BaseURLProvider.server_domain()
 
     where_clauses = [ImageModel.server_domain == server_domain]
 
@@ -210,7 +210,7 @@ def get_predictions(
     count: bool = False,
 ) -> Iterable[Prediction]:
     if server_domain is None:
-        server_domain = settings.BaseURLProvider.api()
+        server_domain = settings.BaseURLProvider.server_domain()
 
     where_clauses = [Prediction.server_domain == server_domain]
 
@@ -249,7 +249,7 @@ def get_image_predictions(
     query = ImagePrediction.select()
 
     if server_domain is None:
-        server_domain = settings.BaseURLProvider.api()
+        server_domain = settings.BaseURLProvider.server_domain()
 
     query = query.switch(ImagePrediction).join(ImageModel)
     where_clauses = [ImagePrediction.image.server_domain == server_domain]
@@ -404,7 +404,7 @@ def get_logo_annotation(
 ) -> Iterable[LogoAnnotation]:
 
     if server_domain is None:
-        server_domain = settings.BaseURLProvider.api()
+        server_domain = settings.BaseURLProvider.server_domain()
 
     query = LogoAnnotation.select().join(ImagePrediction).join(ImageModel)
 

robotoff/cli/main.py
@@ -73,7 +73,7 @@ def regenerate_ocr_insights(
 
     with db:
         import_result = importer.import_insights(
-            predictions, settings.BaseURLProvider.api()
+            predictions, settings.BaseURLProvider.server_domain()
         )
         logger.info(import_result)
 
@@ -208,7 +208,7 @@ def import_insights(
             # Create a new transaction for every batch
             with db.atomic():
                 import_results = importer.import_insights(
-                    prediction_batch, settings.BaseURLProvider.api()
+                    prediction_batch, settings.BaseURLProvider.server_domain()
                 )
                 logger.info(import_results)
 
@@ -245,7 +245,9 @@ def refresh_insights(
     if barcode is not None:
         logger.info(f"Refreshing product {barcode}")
         with db:
-            imported = refresh_insights_(barcode, settings.BaseURLProvider.api())
+            imported = refresh_insights_(
+                barcode, settings.BaseURLProvider.server_domain()
+            )
         logger.info(f"Refreshed insights: {imported}")
     else:
         logger.info("Launching insight refresh on full database")
@@ -272,7 +274,7 @@ def refresh_insights(
                 low_queue,
                 job_kwargs={"result_ttl": 0, "timeout": "5m"},
                 barcodes=barcode_batch,
-                server_domain=settings.BaseURLProvider.api(),
+                server_domain=settings.BaseURLProvider.server_domain(),
             )
 
 
@@ -323,7 +325,7 @@ def import_images_in_db(
                 low_queue,
                 job_kwargs={"result_ttl": 0},
                 batch=batch,
-                server_domain=settings.BaseURLProvider.api(),
+                server_domain=settings.BaseURLProvider.server_domain(),
             )
 
 
@@ -415,7 +417,7 @@ def run_object_detection_model(
                 job_kwargs={"result_ttl": 0},
                 barcode=barcode,
                 image_url=image_url,
-                server_domain=settings.BaseURLProvider.api(),
+                server_domain=settings.BaseURLProvider.server_domain(),
             )
 
 

robotoff/logos.py
@@ -527,7 +527,7 @@ def refresh_nearest_neighbors(day_offset: int = 7, batch_size: int = 500):
                 import_logo_insights(
                     logos,
                     thresholds=thresholds,
-                    server_domain=settings.BaseURLProvider.api(),
+                    server_domain=settings.BaseURLProvider.server_domain(),
                     notify=False,
                 )
 

robotoff/off.py
@@ -390,7 +390,7 @@ def update_product(
     timeout: Optional[int] = 15,
 ):
     if server_domain is None:
-        server_domain = settings.BaseURLProvider.api()
+        server_domain = settings.BaseURLProvider.server_domain()
 
     url = get_product_update_url(server_domain)
 
@@ -458,7 +458,7 @@ def select_rotate_image(
     timeout: Optional[int] = 15,
 ):
     if server_domain is None:
-        server_domain = settings.BaseURLProvider.api()
+        server_domain = settings.BaseURLProvider.server_domain()
 
     url = get_product_image_select_url(server_domain)
     cookies = None

robotoff/scheduler/__init__.py
@@ -100,7 +100,7 @@ def refresh_insights(with_deletion: bool = False):
         .where(
             ProductInsight.annotation.is_null(),
             ProductInsight.timestamp <= datetime_threshold,
-            ProductInsight.server_domain == settings.BaseURLProvider.api(),
+            ProductInsight.server_domain == settings.BaseURLProvider.server_domain(),
         )
         .iterator()
     ):
@@ -218,7 +218,7 @@ def generate_insights():
     with db:
         import_result = import_insights(
             product_predictions_iter,
-            server_domain=settings.BaseURLProvider.api(),
+            server_domain=settings.BaseURLProvider.server_domain(),
         )
     logger.info(import_result)
 

robotoff/scheduler/latent.py
@@ -102,7 +102,7 @@ def get_image_orientation(barcode: str, image_id: str) -> Optional[int]:
         .where(
             Prediction.barcode == barcode,
             Prediction.type == PredictionType.image_orientation.name,
-            Prediction.server_domain == settings.BaseURLProvider.api(),
+            Prediction.server_domain == settings.BaseURLProvider.server_domain(),
             Prediction.source_image.is_null(False),
         )
         .iterator()
@@ -123,7 +123,7 @@ def generate_nutrition_image_insights():
         .where(
             ProductInsight.annotation.is_null(),
             ProductInsight.type == InsightType.nutrition_image.name,
-            ProductInsight.server_domain == settings.BaseURLProvider.api(),
+            ProductInsight.server_domain == settings.BaseURLProvider.server_domain(),
         )
         .execute()
     )
@@ -171,7 +171,8 @@ def generate_nutrition_image_insights():
                         Prediction.type == PredictionType.nutrition_image.name,
                         Prediction.barcode == barcode,
                         Prediction.value_tag == lang,
-                        Prediction.server_domain == settings.BaseURLProvider.api(),
+                        Prediction.server_domain
+                        == settings.BaseURLProvider.server_domain(),
                     )
                     .count()
                 ):

robotoff/settings.py
@@ -32,6 +32,12 @@ def _get_default_scheme() -> str:
     return os.environ.get("ROBOTOFF_SCHEME", "https")
 
 
+def _get_default_domain():
+    # `ROBOTOFF_DOMAIN` can be used to overwrite the Product Opener domain used.
+    # If empty, the domain will be inferred from `ROBOTOFF_INSTANCE`
+    return os.environ.get("ROBOTOFF_DOMAIN", "openfoodfacts.%s" % _instance_tld())
+
+
 class BaseURLProvider(object):
     """BaseURLProvider allows to fetch a base URL for Product Opener/Robotoff.
 
@@ -44,12 +50,8 @@ class BaseURLProvider(object):
         domain: Optional[str] = None,
         scheme: Optional[str] = None,
     ):
-        # `ROBOTOFF_DOMAIN` can be used to overwrite the Product Opener domain used.
-        # If empty, the domain will be inferred from `ROBOTOFF_INSTANCE`
         data = {
-            "domain": os.environ.get(
-                "ROBOTOFF_DOMAIN", "openfoodfacts.%s" % _instance_tld()
-            ),
+            "domain": _get_default_domain(),
             "scheme": _get_default_scheme(),
         }
         if prefix:
@@ -63,6 +65,11 @@ class BaseURLProvider(object):
             return "%(scheme)s://%(prefix)s.%(domain)s" % data
 
         return "%(scheme)s://%(domain)s" % data
+
+    @staticmethod
+    def server_domain():
+        """Return the server domain: `api.openfoodfacts.*`"""
+        return "api." + _get_default_domain()
 
     @staticmethod
     def world():

scripts/insert_images.py
@@ -40,7 +40,7 @@ with db:
                     product.barcode,
                     image_url,
                     product,
-                    settings.BaseURLProvider.api(),
+                    settings.BaseURLProvider.server_domain(),
                 )
             except Exception as e:
                 logger.info("Exception for product {}\n{}".format(product.barcode, e))

tests/integration/insights/test_category_import.py
@@ -79,7 +79,7 @@ class TestCategoryImporter:
             product_store = self.fake_product_store()
         return import_insights(
             predictions,
-            server_domain=settings.BaseURLProvider.api(),
+            server_domain=settings.BaseURLProvider.server_domain(),
             product_store=product_store,
         )
 
@@ -143,7 +143,7 @@ class TestCategoryImporter:
         assert ProductInsight.select().count() == 1
         inserted = ProductInsight.get(ProductInsight.id != insight_id1)
         assert inserted.value_tag == "en:smoked-salmons"
-        assert inserted.server_domain == settings.BaseURLProvider.api()
+        assert inserted.server_domain == settings.BaseURLProvider.server_domain()
         assert not inserted.automatic_processing
 
     def test_import_auto(self):
@@ -157,7 +157,7 @@ class TestCategoryImporter:
         assert ProductInsight.select().count() == 1
         inserted = ProductInsight.get(ProductInsight.id != insight_id1)
         assert inserted.value_tag == "en:smoked-salmons"
-        assert inserted.server_domain == settings.BaseURLProvider.api()
+        assert inserted.server_domain == settings.BaseURLProvider.server_domain()
         assert inserted.automatic_processing
 
     @pytest.mark.parametrize(

tests/integration/insights/test_process_insights.py
@@ -70,7 +70,7 @@ def test_process_insight_category(mocker, peewee_db):
             "comment": f"[robotoff] Adding category 'en:Salmons', ID: {id1}",
         },
         auth=None,
-        server_domain=settings.BaseURLProvider.api(),
+        server_domain=settings.BaseURLProvider.server_domain(),
     )
 
 
@@ -150,7 +150,7 @@ def test_process_insight_update_product_raises(mocker, peewee_db):
             "comment": f"[robotoff] Adding category 'en:Salmons', ID: {id1}",
         },
         auth=None,
-        server_domain=settings.BaseURLProvider.api(),
+        server_domain=settings.BaseURLProvider.server_domain(),
     )
 
     with peewee_db:
@@ -167,7 +167,7 @@ def test_process_insight_update_product_raises(mocker, peewee_db):
             "comment": f"[robotoff] Adding category 'en:Tuna', ID: {id2}",
         },
         auth=None,
-        server_domain=settings.BaseURLProvider.api(),
+        server_domain=settings.BaseURLProvider.server_domain(),
     )
     # we add only two calls
     assert mock.call_count == 2
@@ -206,7 +206,7 @@ def test_process_insight_same_product(mocker, peewee_db):
             "comment": f"[robotoff] Adding category 'en:Big fish', ID: {id2}",
         },
         auth=None,
-        server_domain=settings.BaseURLProvider.api(),
+        server_domain=settings.BaseURLProvider.server_domain(),
     )
     mock.assert_any_call(
         {
@@ -215,5 +215,5 @@ def test_process_insight_same_product(mocker, peewee_db):
             "comment": f"[robotoff] Adding category 'en:Smoked Salmon', ID: {id3}",
         },
         auth=None,
-        server_domain=settings.BaseURLProvider.api(),
+        server_domain=settings.BaseURLProvider.server_domain(),
     )

tests/integration/models_utils.py
@@ -48,7 +48,9 @@ class ProductInsightFactory(UuidSequencer, PeeweeModelFactory):
     n_votes = 0
     value_tag = "en:seeds"
     # we use a lazy function for settings can change in a test
-    server_domain: str = factory.LazyFunction(lambda: settings.BaseURLProvider.api())
+    server_domain: str = factory.LazyFunction(
+        lambda: settings.BaseURLProvider.server_domain()
+    )
     server_type = "off"
     unique_scans_n = 10
     annotation = None
@@ -64,7 +66,9 @@ class PredictionFactory(PeeweeModelFactory):
     data: dict[str, Any] = {}
     timestamp = factory.LazyFunction(datetime.utcnow)
     value_tag = "en:seeds"
-    server_domain = factory.LazyFunction(lambda: settings.BaseURLProvider.api())
+    server_domain = factory.LazyFunction(
+        lambda: settings.BaseURLProvider.server_domain()
+    )
     automatic_processing = None
     predictor = None
     confidence: Optional[float] = None
@@ -92,7 +96,9 @@ class ImageModelFactory(PeeweeModelFactory):
     source_image = factory.Sequence(lambda n: f"/images/{n:02}.jpg")
     width = 400
     height = 400
-    server_domain = factory.LazyFunction(lambda: settings.BaseURLProvider.api())
+    server_domain = factory.LazyFunction(
+        lambda: settings.BaseURLProvider.server_domain()
+    )
     server_type = "off"
 
 

tests/integration/test_api.py
@@ -533,7 +533,7 @@ def test_annotate_insight_anonymous_then_authenticated(client, mocker, peewee_db
         "1",  # barcode
         "en:seeds",  # category_tag
         insight_id=uuid.UUID(insight_id),
-        server_domain=settings.BaseURLProvider.api(),
+        server_domain=settings.BaseURLProvider.server_domain(),
         auth=OFFAuthentication(username="a", password="b"),
     )
 

tests/integration/test_import_image.py
@@ -89,7 +89,7 @@ def test_process_created_logos(peewee_db, mocker):
         ]
         logo_embeddings = [LogoEmbeddingFactory(logo=logo) for logo in logos]
         process_created_logos(
-            image_prediction.id, server_domain=settings.BaseURLProvider.api()
+            image_prediction.id, server_domain=settings.BaseURLProvider.server_domain()
         )
         add_logos_to_ann_mock.assert_called()
         embedding_args = add_logos_to_ann_mock.mock_calls[0].args[1]