f7579508eb60014de223f2e3366cf3d2c09c9659
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-01-27 09:25:20 +0100

    fix: increase MongoDB timeout from 5s to 10s
    We get everyday around 5-20 MongoDB timeout exceptions
    See https://sentry.io/organizations/openfoodfacts/issues/3841214556/?project=1415205&query=is%3Aunresolved&referrer=issue-stream

--- robotoff/mongo.py
+++ robotoff/mongo.py
@@ -5,7 +5,7 @@ from robotoff.utils.cache import CachedStore
 
 
 def get_mongo_client() -> MongoClient:
-    return MongoClient(settings.MONGO_URI, serverSelectionTimeoutMS=5_000)
+    return MongoClient(settings.MONGO_URI, serverSelectionTimeoutMS=10_000)
 
 
 MONGO_CLIENT_CACHE = CachedStore(get_mongo_client, expiration_interval=None)
