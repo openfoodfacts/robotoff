7de37fe922211d451a9e689229d1d881e2113189
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2025-08-21 17:13:15 +0200

    fix: remove local test data stored using git LFS
    Use test-data repo instead

--- tests/unit/prediction/upc_image/test_upc_image.py
+++ tests/unit/prediction/upc_image/test_upc_image.py
@@ -1,32 +1,61 @@
 # import cv2
+# import numpy as np
 # import pytest
+# from openfoodfacts.images import download_image
 
 # from robotoff import settings
 # from robotoff.prediction.upc_image import UPCImageType, find_image_is_upc
-
-# data_dir = settings.TEST_DATA_DIR / "upc_image"
-
 
 # Run UPC detection to detect if the image is dominated by a UPC (and thus
 # should not be a product selected image)
 # UPC detection is buggy since the upgrade to OpenCV 4.10
 # The test is disabled until the issue is resolved
 
+
 # @pytest.mark.parametrize(
-#     "imgpath, expected",
+#     "image_url, expected",
 #     [
-#         (data_dir / "upc1.jpg", UPCImageType.UPC_IMAGE),
-#         (data_dir / "upc2.jpg", UPCImageType.UPC_IMAGE),
-#         (data_dir / "upc3.jpg", UPCImageType.UPC_IMAGE),
-#         (data_dir / "small_upc1.jpg", UPCImageType.SMALL_UPC),
-#         (data_dir / "small_upc2.jpg", UPCImageType.SMALL_UPC),
-#         (data_dir / "small_upc3.jpg", UPCImageType.SMALL_UPC),
-#         (data_dir / "no_upc1.jpg", UPCImageType.NO_UPC),
-#         (data_dir / "no_upc2.jpg", UPCImageType.NO_UPC),
-#         (data_dir / "no_upc3.jpg", UPCImageType.NO_UPC),
+#         (
+#             "https://raw.githubusercontent.com/openfoodfacts/test-data/4a6446c9cac4b279f406fd1e20f8a28b045c823c/robotoff/tests/unit/upc_image/upc1.jpg",
+#             UPCImageType.UPC_IMAGE,
+#         ),
+#         (
+#             "https://raw.githubusercontent.com/openfoodfacts/test-data/4a6446c9cac4b279f406fd1e20f8a28b045c823c/robotoff/tests/unit/upc_image/upc2.jpg",
+#             UPCImageType.UPC_IMAGE,
+#         ),
+#         (
+#             "https://raw.githubusercontent.com/openfoodfacts/test-data/4a6446c9cac4b279f406fd1e20f8a28b045c823c/robotoff/tests/unit/upc_image/upc3.jpg",
+#             UPCImageType.UPC_IMAGE,
+#         ),
+#         (
+#             "https://raw.githubusercontent.com/openfoodfacts/test-data/4a6446c9cac4b279f406fd1e20f8a28b045c823c/robotoff/tests/unit/upc_image/small_upc1.jpg",
+#             UPCImageType.SMALL_UPC,
+#         ),
+#         (
+#             "https://raw.githubusercontent.com/openfoodfacts/test-data/4a6446c9cac4b279f406fd1e20f8a28b045c823c/robotoff/tests/unit/upc_image/small_upc2.jpg",
+#             UPCImageType.SMALL_UPC,
+#         ),
+#         (
+#             "https://raw.githubusercontent.com/openfoodfacts/test-data/4a6446c9cac4b279f406fd1e20f8a28b045c823c/robotoff/tests/unit/upc_image/small_upc3.jpg",
+#             UPCImageType.SMALL_UPC,
+#         ),
+#         (
+#             "https://raw.githubusercontent.com/openfoodfacts/test-data/4a6446c9cac4b279f406fd1e20f8a28b045c823c/robotoff/tests/unit/upc_image/no_upc1.jpg",
+#             UPCImageType.NO_UPC,
+#         ),
+#         (
+#             "https://raw.githubusercontent.com/openfoodfacts/test-data/4a6446c9cac4b279f406fd1e20f8a28b045c823c/robotoff/tests/unit/upc_image/no_upc2.jpg",
+#             UPCImageType.NO_UPC,
+#         ),
+#         (
+#             "https://raw.githubusercontent.com/openfoodfacts/test-data/4a6446c9cac4b279f406fd1e20f8a28b045c823c/robotoff/tests/unit/upc_image/no_upc3.jpg",
+#             UPCImageType.NO_UPC,
+#         ),
 #     ],
 # )
-# def test_find_image_is_upc(imgpath, expected):
-#     image = cv2.imread(str(imgpath))
+# def test_find_image_is_upc(image_url: str, expected: UPCImageType):
+#     response = download_image(image_url, return_struct=True)
+#     arr = np.frombuffer(response.image_bytes, dtype=np.uint8)
+#     image = cv2.imdecode(arr, cv2.IMREAD_COLOR)
 
 #     assert find_image_is_upc(image)[1] == expected
